d5e6960955e77ac5de892297b2cfcd8390e05cd7
ShayanDJ65git config --global user.name ShayanDJ65git config --global user.name ShayanDJ65 <shayanhosseinzadeh1@gmail.com>
2022-09-27 15:16:56 +0330

    First Upload

---
+++ java.js
@@ -0,0 +1,24 @@
+function FirstFunction() {
+    document.getElementById("profile").style.display = "block";
+
+}
+function Secondfunc() {
+    document.getElementById("profile").style.display = "none";
+
+}
+function salam() {
+    alert("Man omadam")
+    console.log("Man omadam")
+}
+function Print() {
+    print();
+}
+function Rotate(){
+    document.getElementById("profile").style.rotate(90)
+}
+function test(x,y) {
+    return x* y 
+}
+const car = {type:"Pride", Model:"131", color:"white"}
+
+alert(car.type)
